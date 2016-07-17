1240a11153e52592277cecac6e31e0ed2322242b
Zazz <captain.zazz@gmail.com>
2016-07-17 21:33:09 +0100

    Update comments on NotificationsRealtime

--- src/realtime/api/notifications/NotificationsRealtime.js
+++ src/realtime/api/notifications/NotificationsRealtime.js
@@ -12,8 +12,9 @@ class NotificationsRealtime extends AbstractRealtimeAPI {
     }
 
     /**
-     * TODO
+     * TODO What is this for? No data to pass?
      * @param {function} callback
+     * @example onClearUnreadFlag(function(){ ... });
      */
     onClearUnreadFlag(callback){
         this.service.on('notifications:clear_unread_flag', function(){
@@ -23,8 +24,9 @@ class NotificationsRealtime extends AbstractRealtimeAPI {
     }
 
     /**
-     * TODO Split this up into different methods by Notification Method Type
-     * @param {function} callback
+     * Callback called when a notification has been read
+     * @param {function} callback Accepts the notification type and key strings
+     * @example onMarkRead(function(notificationType, notificationKey){ ... });
      */
     onMarkRead(callback){
         this.service.on('notifications:mark_read', function(data){
@@ -39,14 +41,14 @@ class NotificationsRealtime extends AbstractRealtimeAPI {
             var notificationType = split[0];
             var notificationKey = split[1];
 
-            console.info('notifications:mark_read', arguments);
             callback(notificationType, notificationKey);
         });
     }
 
     /**
-     * Called when marking all notifications as read
+     * Callback called when marking all notifications as read
      * @param {function} callback Takes nothing
+     * @example onMarkReadAll(function(){ ... });
      */
     onMarkReadAll(callback){
         this.service.on('notifications:mark_all_read', function(){
@@ -58,23 +60,18 @@ class NotificationsRealtime extends AbstractRealtimeAPI {
     /**
      * Callback is applied when a new notification is created
      * @param {function} callback Takes an array of NotificationModels in the shortlist
+     * @example onNew(function(notifications){ ... });
      */
     onNew(callback){
         this.service.on('notifications:new', function(data){
-            /**
-             * All notifications in list, both read and unread
-             * TODO Convert to Notification Models
-             * @type {NotificationModel[]}
-             */
             try {
                 var notifications = data.notifications.map((obj)=> {
                     return new NotificationModel(obj);
                 });
+                callback(notifications);
             }catch(e){
                 console.error("NotificationsRealtime.onNew Error:", e);
             }
-
-            callback(notifications);
         });
     }
 }
